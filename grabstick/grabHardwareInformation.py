0d03412b14bc889c96fb8e1cc9b3db5e065f0f2a
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-03 20:34:20 +0100

    Basic version of gethardwareinfo

---
+++ grabstick/grabHardwareInformation.py
@@ -0,0 +1,31 @@
+import platform
+import socket
+import re
+import uuid
+import json
+# import psutil
+import logging
+
+
+# https://stackoverflow.com/a/58420504
+
+def getSystemInfo():
+    try:
+        info = {}
+        info['platform'] = platform.system()
+        info['platform-release'] = platform.release()
+        info['platform-version'] = platform.version()
+        info['architecture'] = platform.machine()
+        info['hostname'] = socket.gethostname()
+        info['ip-address'] = socket.gethostbyname(socket.gethostname())
+        info['mac-address'] = ':'.join(re.findall('..',
+                                                  '%012x' % uuid.getnode()))
+        info['processor'] = platform.processor()
+        # info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3)))+" GB"
+        return json.dumps(info)
+    except Exception as e:
+        logging.exception(e)
+
+
+if __name__ == "__main__":
+    print(getSystemInfo())
