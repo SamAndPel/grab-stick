a0d78b28a825fe532b9ee99bad0314a4a3c569a7
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-16 14:14:17 +0100

    Add detail to psutil exclusion

--- grabstick/grabHardwareInformation.py
+++ grabstick/grabHardwareInformation.py
@@ -6,8 +6,10 @@ import json
 # import psutil
 import logging
 
+# Psutil is not a standard library module so trying to avoid using it for now
 
 # https://stackoverflow.com/a/58420504
+
 
 def getSystemInfo():
     try:
@@ -18,8 +20,7 @@ def getSystemInfo():
         info['architecture'] = platform.machine()
         info['hostname'] = socket.gethostname()
         info['ip-address'] = socket.gethostbyname(socket.gethostname())
-        info['mac-address'] = ':'.join(re.findall('..',
-                                                  '%012x' % uuid.getnode()))
+        info['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
         info['processor'] = platform.processor()
         # info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3)))+" GB"
         return json.dumps(info)
