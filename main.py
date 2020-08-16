a9c1a28e3a2e36a46e194ed65cd296e305e45206
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-16 14:14:29 +0100

    Refactor structure, begin work on main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# GRABSTICK - A python utility to get as much system information as possible in
+# as little time as possible. Designed to be run using a USB/rubberducky in an
+# ethical hacking environment, but use as you like.
+
+from grabstick import *
+
+
+def main():
+    # Run main() of each getter library (in ./grabstick)
+
+    # Concatenate results
+
+    # Render as XML, HTML, TXT for future usage
+
+
+if __name__ == "__main__":
+    main()
