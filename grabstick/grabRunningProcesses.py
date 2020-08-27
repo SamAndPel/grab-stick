ec15d9b0a2de20b1f7c78337fbe35a8fef1c1363
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-27 16:37:57 +0100

    Grab running processes script

--- grabstick/grabRunningProcesses.py
+++ grabstick/grabRunningProcesses.py
@@ -1,2 +1,28 @@
 import os
 import psutil
+import time
+from datetime import datetime
+
+def get_running_processes():
+    # Iterate over all running process
+    processes = []
+    for proc in psutil.process_iter():
+        try:
+            procinfo = {}
+            # Get process info
+            procinfo["name"] = proc.name()
+            procinfo["pid"] = proc.pid
+            procinfo["cpu"] = proc.cpu_percent()
+            procinfo["ram"] = str(round((proc.memory_full_info().uss / (1024.0 ** 2)), 3)) + "mb"
+            procinfo["user"] = proc.username()
+            procinfo["status"] = proc.status()
+            procinfo["priority"] = int(proc.nice())
+            procinfo["threads_spawned"] = proc.num_threads()
+
+            processes.append(procinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return processes
+
+def main():
+    return get_running_processes()
