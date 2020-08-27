5e36a9798d3197d28a05f939168cffcd9e3ecbb1
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-27 17:17:14 +0100

    Complete grabgroups (lists members of each group)

--- grabstick/grabGroups.py
+++ grabstick/grabGroups.py
@@ -1,13 +1,20 @@
 import subprocess
 
+def get_members_of_group(group):
+    #returns list
+    result = str(subprocess.check_output(f'net localgroup "{group}"'))
+    section = result[result.find("Members\\r\\n") + 7:result.find("The command completed successfully."):].split("\\r\\n")
+    members = section[3:-1:]
+    return [member.strip() for member in members]
 
 def get_groups():
     groups = str(subprocess.check_output("net localgroup")).split("\\r\\n")[4:-3:]
-    return groups
+    groups_formatted = [group[1::] for group in groups]
+    groupdict = {}
+    for group in groups_formatted:
+        groupdict[group] = get_members_of_group(group)
+    return groupdict
 
 
 def main():
     return get_groups()
-
-if __name__ == "__main__":
-    print(get_groups())
