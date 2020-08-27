52e31e45e0bb23e7fc8d7534fcf4cedc04ed0a4f
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2020-08-27 15:41:45 +0100

    GrabHardwareInformation now conforms to output schema

--- grabstick/grabHardwareInformation.py
+++ grabstick/grabHardwareInformation.py
@@ -20,13 +20,14 @@ def getSystemInfo():
         info['architecture'] = platform.machine()
         info['hostname'] = socket.gethostname()
         info['ip-address'] = socket.gethostbyname(socket.gethostname())
-        info['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['mac-address'] = ':'.join(re.findall('..',
+                                                  '%012x' % uuid.getnode()))
         info['processor'] = platform.processor()
         # info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3)))+" GB"
-        return json.dumps(info)
+        return info
     except Exception as e:
         logging.exception(e)
 
 
-if __name__ == "__main__":
-    print(getSystemInfo())
+def main():
+    return getSystemInfo()
